6bd64493c7dd992dc7c388d3dc9d4093ac343c23
TomMain <TVMain@TRV-MacBook-Pro.local>
2019-09-09 12:23:06 -0500

    functionality added

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,14 @@
 
-
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
+
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(127) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP DEFAULT now(),
-	PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
-
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
 
+USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
-INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
-INSERT INTO burgers (burger_name) VALUES ('Beyond Meat Burger');
-
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("impossible hamburger", false), ("impossible cheeseburger", false), ("beyond meat burger", false);
